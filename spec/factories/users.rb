b62efc2af9e25c43df6182395b48bef5842912bd
Kornelije Sajler <xajler@gmail.com>
2012-11-05 15:02:47 +0100

    Added format documentation for RSpec and User factory changed

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     email 'xajler@gmail.com'
     password 'x1234567'
-    password_digest 'x1234567'
+    password_confirmation 'x1234567'
     full_name 'Kornelije Sajler'
   end
 end
